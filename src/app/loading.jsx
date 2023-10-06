8f5ca0b57b052d8703c6282deb7a03c074af0f85
arey-pranay <pranayparikh2004@gmail.com>
2023-10-06 18:04:18 +0530

    added loading effect so conveniently...

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-44 h-36">
+      <img src="loadingTriangle.svg" alt="loading" />
+    </div>
+  );
+}
